29080eabcede05a2b66c169e3ebd40225879fa3b
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-03-05 00:01:53 -0400

    refactor: se cambio la funcion en una tabla

--- src/romanos.js
+++ src/romanos.js
@@ -1,10 +1,21 @@
 function decimalARomano(numero) {
-    if (numero >= 5) return "V" + decimalARomano(numero - 5);
-    
+    const valores = [
+        { num: 500, rom: "D" },
+        { num: 100, rom: "C" },
+        { num: 50, rom: "L" },
+        { num: 10, rom: "X" },
+        { num: 9, rom: "IX" },
+        { num: 5, rom: "V" },
+        { num: 4, rom: "IV" },
+        { num: 1, rom: "I" }
+    ];
+
     let resultado = "";
-    while (numero > 0) {
-        resultado += "I";
-        numero--;
+    for (let i = 0; i < valores.length; i++) {
+        while (numero >= valores[i].num) {
+            resultado += valores[i].rom;
+            numero -= valores[i].num;
+        }
     }
     return resultado;
 }
