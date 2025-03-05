5b6be69afa1bc21743061122369f2c2ea841699a
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-03-04 23:53:21 -0400

    feat: se agrego la funcionalidad de convertir el 2

--- src/romanos.js
+++ src/romanos.js
@@ -1,5 +1,6 @@
 function decimalARomano(numero) {
     if (numero === 1) return "I";
+    if (numero === 2) return "II";
 }
 
 export default decimalARomano;
--- src/romanos.test.js
+++ src/romanos.test.js
@@ -4,4 +4,9 @@ describe("Conversión de números decimales a romanos", () => {
     it("Convierte 1 a 'I'", () => {
         expect(decimalARomano(1)).toEqual("I");
     });
+
+    it("Convierte 2 a 'II'", () => {
+        expect(decimalARomano(2)).toEqual("II");
+    });
+    
 });
